d6f321a563c917c593435f3424fc72ef3da8c208
DeniseFafette <denisefafette@gmail.com>
2020-01-24 11:55:39 -0800

    updated taskModal.js

---
+++ api/tasks/taskModal.js
@@ -0,0 +1,16 @@
+const db = require('../dbConfig')
+
+module.exports = {
+    find,
+    add
+}
+
+function find() {
+    return db('tasks')
+        .join('projects', 'project_id', 'projects.id')
+        .select('task_description', 'notes', 'tasks.id', 'task_completed', 'projects.id', 'name', 'description')
+}
+
+function add(task) {
+    return db('tasks').insert(task)
+}
