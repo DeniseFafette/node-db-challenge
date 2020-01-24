427b254b1f23e0a334df093e189dfb57bff61327
DeniseFafette <denisefafette@gmail.com>
2020-01-24 11:45:03 -0800

    updated taskRouter.js

--- api/tasks/taskRouter.js
+++ api/tasks/taskRouter.js
@@ -5,11 +5,24 @@ const tasks = require('./taskModal')
 router.get('/', (req, res) => {
     tasks.find()
         .then(list => {
-            res.status(200).json(list)
+            const { task_description, notes, id, task_completed, project_id, name, description } = list[0]
+            const taskComplete = task_completed ? true : false
+            const response = {task_description, notes, id, task_completed: taskComplete, project:{project_id, name, description}}
+            res.status(200).json(response)
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({error: 'Failed to retrieve projects'})
+        })
+})
+
+router.post('/', (req, res) => {
+    tasks.add(req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Could not add task'})
         })
 })
 
