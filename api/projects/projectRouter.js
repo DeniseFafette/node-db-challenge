5f01266f8f607746e237e2c1a32a46b4c4d5c105
DeniseFafette <denisefafette@gmail.com>
2020-01-24 11:50:34 -0800

    updated projectRouter.js

--- api/projects/projectRouter.js
+++ api/projects/projectRouter.js
@@ -5,11 +5,24 @@ const projects = require('./projectModal')
 router.get('/', (req, res) => {
     projects.find()
         .then(list => {
-            res.status(200).json(list)
+            const { id, name, description, completed } = list[0]
+            const isComplete = completed ? true : false
+            res.status(200).json({id, name, description, completed: isComplete})
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({error: 'Failed to retrieve projects'})
+        })
+})
+
+router.post('/', (req, res) => {
+    projects.add(req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'Failed to post project'})
         })
 })
 
