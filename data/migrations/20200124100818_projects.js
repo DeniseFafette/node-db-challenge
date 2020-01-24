488da1e0cd76f03d125a43c97f7964354ffe611f
DeniseFafette <denisefafette@gmail.com>
2020-01-24 13:37:51 -0800

    deleted task and resource migration file and chained the tables into a single projects migration file

--- data/migrations/20200124100818_projects.js
+++ data/migrations/20200124100818_projects.js
@@ -6,18 +6,41 @@ exports.up = function(knex) {
       tbl.text('description')
       tbl.boolean('completed').defaultTo(false)
   })
-  .createTable('project_resource', tbl => {
+  .createTable('resources', tbl => {
+    tbl.increments()
+    tbl.string('resource_name', 24).notNullable().unique()
+    tbl.text('resource_description')
+})
+.createTable('tasks', tbl => {
+    tbl.increments()
+    tbl.text('task_description').notNullable()
+    tbl.text('notes')
+    tbl.boolean('task_completed').defaultTo(false)
+    tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+})
+.createTable('project_resource', tbl => {
     tbl.increments()
     tbl.integer('project_id')
         .unsigned()
         .notNullable()
         .references('id')
-        inTable('projects')
+        .inTable('projects')
+    tbl.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('resources')
   })
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists('project_resource')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
     .dropTableIfExists('projects')
 };
