5207e7a6a7ce0e2761b19915c5843ca9797b38c2
DeniseFafette <denisefafette@gmail.com>
2020-01-24 10:06:24 -0800

    created api folder for projects resources tasks modal and router files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const server = express()
 const projectRouter = require('./projects/projectRouter')
+const resourceRouter = require('./resources/resourceRouter')
+const taskRouter = require('./tasks/taskRouter')
 
 server.use('express.json()')
 server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
 
 module.exports = server;
