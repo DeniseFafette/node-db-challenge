297cbd681da63813d3edf398139194b6030b988d
DeniseFafette <denisefafette@gmail.com>
2020-01-24 10:32:16 -0800

    updated project, resourc and task Modal files

---
+++ api/projects/projectModal.js
@@ -0,0 +1,14 @@
+const db = require('../dbConfig')
+
+module.exports = {
+    find,
+    add
+}
+
+function find() {
+    return db('projects')
+}
+
+function add() {
+    return db('projects').insert(project)
+}
---
+++ api/resources/resourceModal.js
@@ -0,0 +1,14 @@
+const db = require('../dbConfig')
+
+module.exports = {
+    find,
+    add
+}
+
+function find() {
+    return db('resources')
+}
+
+function add() {
+    return db('resources').insert(resource)
+}
