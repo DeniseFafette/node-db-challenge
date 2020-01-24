03e81c9155b747f14cdee6b57de93d8903ab508f
DeniseFafette <denisefafette@gmail.com>
2020-01-24 11:06:26 -0800

    updated migration tables for projects resources and taks tables

--- data/migrations/20200124100818_projects.js
+++ data/migrations/20200124100818_projects.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments()
+      tbl.string('name', 24).notNullable().unique()
+      tbl.text('description')
+      tbl.boolean('completed').defaultTo(false)
+  })
+  .createTable('project_resource', tbl => {
+    tbl.increments()
+    tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        inTable('projects')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project_resource')
+    .dropTableIfExists('projects')
 };
--- data/migrations/20200124100842_resources.js
+++ data/migrations/20200124100842_resources.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('resources', tbl => {
+        tbl.increments()
+        tbl.string('resource_name', 24).notNullable().unique()
+        tbl.text('resource_description')
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('resources')
 };
